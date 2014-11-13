0753d16d8f65af06b8edc1b66f0ef1956054e8bb
smetkat <smetkat@gmail.com>
2014-11-13 22:38:26 +0100

    odladene parametry pri ukladani a rozpoznani gesta

--- src/cz/vutbr/fit/mogger/Gesture.java
+++ src/cz/vutbr/fit/mogger/Gesture.java
@@ -88,13 +88,12 @@ public class Gesture {
             }
             else {
                 // uzivatel zacal vytvaret gesto
-                if ((abs(x - prev_x) + abs(y - prev_y) + abs(z - prev_z)) > 2) {
-                    clear();
-                    sounds.PlayTone();
-                    mogger.textView.setText("Recording ...");
+                if ((abs(x - prev_x) + abs(y - prev_y) + abs(z - prev_z)) > 1) {
                     ref_coord_x.add(x);
                     ref_coord_y.add(y);
                     ref_coord_z.add(z);
+                    mogger.textView.setText("Recording ...");
+                    sounds.PlayTone();
                 }
                 prev_x = x;
                 prev_y = y;
@@ -109,8 +108,8 @@ public class Gesture {
             // mame alespon 8 vektoru, zacneme overovat, zda uzivatel neukoncil gesto
             if (ref_coord_x.size() >= 8) {
                 if ((abs(x - prev_x) + abs(y - prev_y) + abs(z - prev_z)) < 3) {
-                    sounds.PlayTone();
                     mogger.fastestListener.stopRecording();
+                    sounds.PlayTone();
                     mogger.button2.setText("Save");
                     mogger.button2.setTag(1);
                     mogger.textView.setText("New gesture saved.");
